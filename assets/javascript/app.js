f25356f03c655ee3edc2a2745247b46b21cd6126
Charlie Parsons <cdaltonparsons@gmail.com>
2019-06-10 08:52:24 -0600

    moved questions from HTML hard code into the javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+var myQuestions = [
+    {
+      question: "What is the name of Tom Cruise's wingman in the critically acclaimed film, 'Top Gun'",
+      answers: {
+        a: "Rabbit",
+        b: "Goose",
+        c: "Steve",
+        d: "Ice Man"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which of the following is NOT  like the others?",
+      answers: {
+        a: "KISS",
+        b: "Motley Crue",
+        c: "Little River Band",
+        d: "Poison"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "In which 80's cop drama did Don Johnson play the leading man, Detective Sonny Crockett?",
+      answers: {
+        a: "Miami Vice",
+        b: "Hawaii Five-O",
+        c: "CHiPs",
+        d: "Starsky and Hutch"
+      },
+      correctAnswer: "a"
+    },
+    {
+        question: "Who played Axel Foley in the super smash hit 'Beverly Hills Cop'"
+    }
+  ];
